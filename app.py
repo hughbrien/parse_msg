541ad37ddeb3fd6b64c0c8d748be79dd3d8d6278
Hugh Brien <hbrien@cisco.com>
2024-06-11 16:02:44 -0400

    Updated to parse_file

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
+
 import extract_msg
 
-f = r'MS_Outlook_file.msg'  # Replace with yours
-msg = extract_msg.Message(f)
-msg_sender = msg.sender
-msg_date = msg.date
-msg_subj = msg.subject
-msg_message = msg.body
-msg.close()
+def parse_file(file_name):
+    alert_message = file_name
+    print(alert_message)
+    msg = extract_msg.Message(alert_message)
+    msg_sender = msg.sender
+    msg_date = msg.date
+    msg_subj = msg.subject
+    msg_message = msg.body
+    msg.close()
+    print('Sender: {}'.format(msg_sender))
+    print("")
+    print('Sent On: {}'.format(msg_date))
+    print("")
+    print('Subject: {}'.format(msg_subj))
+    print("")
+    print('Body: {}'.format(msg_message))
 
-print('Sender: {}'.format(msg_sender))
-print('Sent On: {}'.format(msg_date))
-print('Subject: {}'.format(msg_subj))
-print('Body: {}'.format(msg_message))
+if __name__ == '__main__':
+    my_file = "/Users/hbrien/Software/appd_alert_outlook_messages/Warning events detected for Register Email Alerts! (995).msg"
+    parse_file(my_file)
